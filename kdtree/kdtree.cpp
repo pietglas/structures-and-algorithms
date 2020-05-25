a26de43cd9597c2f99ddb97980af8b33390777f7
piet@piet-Latitude-E7440 <pietglas77@gmail.com>
2020-05-25 15:30:03 +0200

    mad new local repo

--- kdtree/kdtree.cpp
+++ kdtree/kdtree.cpp
@@ -1,18 +1,37 @@
 #include <array>
 
-template<size_t N, typename Type>
+template<size_t N, typename T>
 class KDTree {
-public:
+private:
 	struct Node {
-		Node()
-		Node(std::array coordinates, )
-		std::array coordinates_;
+		Node(std::array<double> coordinates, T data) : coordinates_{coordinates},
+			data_{data} {}
+		std::array<double> coordinates_;
+		T data_;
+		Node * left_;
+		Node * right_;
 	};
+public:
 	KDTree();
 	~KDTree();
 
 	KDTree(const KDTree & rhs);
 	KDTree & operator =(const KDTree & rhs);
 
-	void add()
+	void add(std::array<double> coordinates, T data);
+	std::array<double> containsData(T data);
+	bool containsCoordinates(std::array<double> coordinates);
+
+private:
+	Node * root_;
 };
+
+template<size_t N, typename T>
+KDTree<N, T>::KDTree() : root_{nullptr} {}
+
+template<size_t N, typename T>
+KDTree<N, T>::~KDTree() {
+
+}
+
+
