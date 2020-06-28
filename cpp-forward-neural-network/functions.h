ba4edab82fcc02ef1a512a9fb8e70ce08258f98b
piet@piet-Latitude-E7440 <pietglas77@gmail.com>
2020-06-28 23:13:37 +0200

    argument sigmoid is now const ref

--- cpp-forward-neural-network/functions.h
+++ cpp-forward-neural-network/functions.h
@@ -6,11 +6,11 @@ using Eigen::Matrix;
 using Vector = Eigen::VectorXd;
 
 /* sigmoid function as per (ch 1, 3) */
-Vector sigmoid(Vector& z) {
-	const int size = z.size();
-	for (int i = 0; i != size; ++i)
-		z(i) = 1 / (1 +  exp(z(i)));
-	return z;
+Vector sigmoid(const Vector& z) {
+	Vector result(z.size());
+	for (int i = 0; i != z.size(); ++i)
+		result(i) = 1 / (1 +  exp(z(i)));
+	return result;
 }
 
 /* the derivative of the sigmoid function */
@@ -19,11 +19,11 @@ Vector sigmoidPrime(Vector& z) {
 }
 
 /* calculates the coefficient-wise product of two column vectors
-   of equal size */
+   of equal size. Throws std::invalid_argument if the vectors
+   do not have the same size */
 Vector coeffProduct(const Vector& v1, const Vector& v2) {
 	if (v1.size() != v2.size())
 		throw std::invalid_argument("vectors not of same size");
-	
 	Matrix<double,  v1.size(), 1> product;
 	for (int i = 0; i != v1.size(); ++i)
 		product(i) = v1(i) * v2(i);
